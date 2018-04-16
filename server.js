4defe9632db563ecde20780c3ff09e0c02cf12c0
WhiteAce <whiteace@yeah.net>
2018-04-16 15:20:13 +0800

    hell

---
+++ server.js
@@ -0,0 +1,8 @@
+import http from 'http'
+
+http
+  .createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.write('Hello World')
+    response.end()
+  }).listen(8080)
